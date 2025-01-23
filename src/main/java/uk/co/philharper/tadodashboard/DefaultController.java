3d84bf77015919cf54b82d19af0c31909049c2a5
Phil Harper <phil.harper93@gmail.com>
2025-01-23 21:06:42 +0000

    Set login as default url

---
+++ src/main/java/uk/co/philharper/tadodashboard/DefaultController.java
@@ -0,0 +1,13 @@
+package uk.co.philharper.tadodashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login";
+    }
+}
