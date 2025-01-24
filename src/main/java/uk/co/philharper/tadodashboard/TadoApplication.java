257b4305b9f4dd8734add0624a1880f1ffadc69d
Phil Harper <phil.harper93@gmail.com>
2025-01-24 08:23:48 +0000

    Revert "Include spring security"
    This reverts commit f501f0e4cb38032e8ae2a76eb102d7413e65520f.

--- src/main/java/uk/co/philharper/tadodashboard/TadoApplication.java
+++ src/main/java/uk/co/philharper/tadodashboard/TadoApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableWebSecurity
 public class TadoApplication {
 
 	public static void main(String[] args) {
